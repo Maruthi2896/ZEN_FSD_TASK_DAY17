627ef87da3c75106034841686ba4eb11df770003
Maruthi K J <123438736+Maruthi2896@users.noreply.github.com>
2023-07-29 16:02:14 +0530

    Add files via upload
    ok

---
+++ script.js
@@ -0,0 +1,44 @@
+let timer1 = (num) => {
+    let count = num;
+    const content = document.querySelector("#countdown");
+    setTimeout(() => {
+      content.innerHTML = count--;
+      setTimeout(() => {
+        content.innerHTML = count--;
+        setTimeout(() => {
+          content.innerHTML = count--;
+          setTimeout(() => {
+            content.innerHTML = count--;
+            setTimeout(() => {
+              content.innerHTML = count--;
+              setTimeout(() => {
+                content.innerHTML = count--;
+                setTimeout(() => {
+                  content.innerHTML = count--;
+                  setTimeout(() => {
+                    content.innerHTML = count--;
+                    setTimeout(() => {
+                      content.innerHTML = count--;
+                      setTimeout(() => {
+                        content.innerHTML = count--;
+                        setTimeout(() => {
+                          content.innerHTML = "Happy Indipendance Day";
+                        }, 1000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  };
+  
+  let maruthi1 = (num, callback) => {
+    callback(num);
+  };
+  
+  maruthi1(10, timer1);
+  
